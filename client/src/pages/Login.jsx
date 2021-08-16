8c65aa343e10a95eb14fed77d3bb0d6fc28f34a9
Zdenda <zdenda.rotal@seznam.cz>
2021-08-16 18:46:45 +0200

    vylepseni v Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -6,23 +6,30 @@ import {CircularProgress} from "@material-ui/core"
 const Login = ({inputNumber}) => {
     const [waiting, setWaiting] = useState(false);
     const textUser = useRef();
-    const [user, setUser] = useState();
     const [error, seterror] = useState(false);
+    const [emptyError, setEmptyError] = useState(false);
     const history = useHistory();
     
 
     const setUserMethod = async() => {
-            setWaiting(true);
-            try {
-                const dataUser = await axios(`https://gorest.co.in/public/v1/users/${textUser.current.value}`);
-                console.log(dataUser.data.data);
-                setUser(dataUser.data.data);
-                inputNumber(dataUser.data.data);
-                history.push("/");
-            } catch (err) {
-                seterror(true);
-                setWaiting(false);
+            if (textUser.current.value !== "") {
+                setEmptyError(false);
+                setWaiting(true);
+                try {
+                    
+                    const dataUser = await axios(`https://gorest.co.in/public/v1/users/${textUser.current.value}`);
+                    console.log(dataUser.data.data);
+                    inputNumber(dataUser.data.data);
+                    history.push("/");
+                } catch (err) {
+                    seterror(true);
+                    setWaiting(false);
+                }
+            } else {
+                seterror(false);
+                setEmptyError(true);
             }
+            
            
     }
     return (
@@ -33,7 +40,8 @@ const Login = ({inputNumber}) => {
                 <button onClick={setUserMethod} className="mainBtn">login</button>
             </div>
             )}
-            <span style={{color: "red"}}>{error && "chyba nenasel jsem uzivatele s timto cislem"}</span>
+            <span style={{color: "red"}}>{error && "chyba nenašel jsem uživatele s tímto číslem"}</span>
+            <span style={{color: "red"}}>{emptyError && "zadejte prosím číslo"}</span>
         </div>
     )
 }
